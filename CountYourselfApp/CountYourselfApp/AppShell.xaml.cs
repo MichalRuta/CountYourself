a57e852f50bc7c8334c66c617208952b15d6d940
Michał Ruta <michal.ruta@sage.com>
2021-12-09 20:55:56 +0100

    Frist commit

---
+++ CountYourselfApp/CountYourselfApp/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using CountYourselfApp.ViewModels;
+using CountYourselfApp.Views;
+using Xamarin.Forms;
+
+namespace CountYourselfApp
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
---
+++ CountYourselfApp/CountYourselfApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using CountYourselfApp.ViewModels;
+
+namespace CountYourselfApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
