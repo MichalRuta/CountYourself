9328da11192ab883712ea70e63f5c7e5d8cd31a7
Michal Ruta <michal.ruta@sage.com>
2022-06-16 23:17:51 +0200

    First view of WebApp

---
+++ src/CountYourself.Service/CountYourself.WebApp/Models/BillRaportModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace CountYourself.WebApp.Models
+{
+	public class BillRaportModel
+	{
+		public List<BillModel> Bills { get; set; } = new List<BillModel>();
+
+        public decimal SumOfValues
+        {
+            get
+            {
+                return Bills.Sum(x => x.Value);
+            }
+        }
+	}
+}
+
